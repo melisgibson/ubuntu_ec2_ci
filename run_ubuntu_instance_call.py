80e494af68980bbbb30f75b58e6bed8be48a3066
Zaire Ali <zaireali649@gmail.com>
2022-07-06 19:48:20 -0400

    Update run_ubuntu_instance_call.py

--- run_ubuntu_instance_call.py
+++ run_ubuntu_instance_call.py
@@ -1,5 +1,9 @@
+import os
 from run_ubuntu_instance import *
 
-client = boto3.client('ec2')
+client = boto3.client('ec2',
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
+    aws_secret_access_key=os.environ['AWS_SECRET_KEY'],
+)
 
 create_apache_ec2(client, KeyName="private-ec2", SecurityGroups=["launch-wizard-6"])
